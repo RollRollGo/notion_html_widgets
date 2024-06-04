d5ff9f4d4ab5232faa550c546b62d47a8a7b138e
ShaoyaHUANG <shaoyahuang22@gmail.com>
2024-06-04 13:44:47 +0800

    fix: change logo image into toggle button

--- answer_machine/script.js
+++ answer_machine/script.js
@@ -1,14 +1,17 @@
 document.addEventListener('DOMContentLoaded', initialize);
 
 const QUOTE_ELEMENT = document.getElementById('quote');
+const OVERLAY_TEXT = document.getElementById('overlay-text');
+const TOGGLE_BUTTON = document.getElementById('toggle-button');
+const LOGO_IMAGE = document.getElementById('logo');
+let isQuoteDisplayed = false;
 
-function fetchQuotes() {
+function fetchQuotes(callback) {
 	fetch('answers.json')
 		.then((response) => response.json())
 		.then((data) => {
 			const answers = extractAnswersFromJsonPair(data);
-			const randomIndex = getRandomIndex(answers);
-			QUOTE_ELEMENT.textContent = answers[randomIndex];
+			callback(answers);
 		})
 		.catch((error) => console.error('Error fetching answers:', error));
 }
@@ -21,6 +24,36 @@ function getRandomIndex(quotes) {
 	return Math.floor(Math.random() * quotes.length);
 }
 
+function showRandomQuotes(answers) {
+	const randomIndex = getRandomIndex(answers);
+	QUOTE_ELEMENT.textContent = answers[randomIndex];
+	QUOTE_ELEMENT.style.display = 'inline';
+	TOGGLE_BUTTON.innerHTML = 'BACK';
+}
+
+function hideOverlay() {
+	OVERLAY_TEXT.style.display = 'none';
+}
+
+function showOverlay() {
+	OVERLAY_TEXT.style.display = 'inline';
+	QUOTE_ELEMENT.style.display = 'none';
+	TOGGLE_BUTTON.innerHTML =
+		'<img id="logo" src="../assets/logo.png" alt="rollrollgo" />';
+}
+
+function handleButtonClick() {
+	if (isQuoteDisplayed) {
+		showOverlay();
+	} else {
+		fetchQuotes((answers) => {
+			showRandomQuotes(answers);
+			hideOverlay();
+		});
+	}
+	isQuoteDisplayed = !isQuoteDisplayed;
+}
+
 function initialize() {
-	fetchQuotes();
+	TOGGLE_BUTTON.addEventListener('click', handleButtonClick);
 }
