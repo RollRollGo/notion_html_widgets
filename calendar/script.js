c6b12b007a99546767fb858d5c8f471aeda65513
ShaoyaHUANG <shaoyahuang22@gmail.com>
2024-07-08 18:33:43 +0800

    fix: add click hint, change date page display

--- calendar/script.js
+++ calendar/script.js
@@ -1,8 +1,8 @@
-function getSolarDateInfo(today) {
+function getSolarDateInfo(date) {
 	return {
-		month: today.toLocaleDateString('zh-CN', { month: 'long' }),
-		day: today.getDate(),
-		weekday: today.toLocaleDateString('zh-CN', { weekday: 'long' }),
+		month: date.toLocaleDateString('zh-CN', { month: 'long' }),
+		day: date.getDate(),
+		weekday: date.toLocaleDateString('zh-CN', { weekday: 'long' }),
 	};
 }
 
@@ -10,25 +10,37 @@ function getLunarDateInfo(lunar) {
 	return {
 		month: `农历${lunar.getMonthInChinese()}月`,
 		day: lunar.getDayInChinese(),
-		weekday: `星期${lunar.getWeekInChinese()}`,
 	};
 }
 
-function displayDateInfo(dateInfo) {
-	Object.keys(dateInfo).forEach((key) => {
-		document.getElementById(key).textContent = dateInfo[key];
+function displayCardInfo(info) {
+	const DATE_PAGE = document.getElementById('date');
+	DATE_PAGE.innerHTML = '';
+
+	Object.keys(info).forEach((key) => {
+		const childElement = document.createElement('p');
+		childElement.id = key;
+		childElement.textContent = info[key];
+		DATE_PAGE.appendChild(childElement);
 	});
+
+	if (info.month.includes('农历')) {
+		const childElement = document.createElement('img');
+		childElement.id = 'logo';
+		childElement.src = '../assets/logo.png';
+		DATE_PAGE.appendChild(childElement);
+	}
 }
 
 function initializeEventListeners(solarDate, lunarDate) {
 	const CONTAINER = document.getElementById('container');
 
 	CONTAINER.addEventListener('mousedown', () => {
-		displayDateInfo(lunarDate);
+		displayCardInfo(lunarDate);
 	});
 
 	CONTAINER.addEventListener('mouseup', () => {
-		displayDateInfo(solarDate);
+		displayCardInfo(solarDate);
 	});
 }
 
@@ -39,10 +51,10 @@ function initialize() {
 	const SOLAR_DATE = getSolarDateInfo(TODAY);
 	const LUNAR_DATE = getLunarDateInfo(LUNAR);
 
-	displayDateInfo(SOLAR_DATE);
+	displayCardInfo(SOLAR_DATE);
 
-	document.getElementById('yi-list').textContent = `${LUNAR.getDayYi()}`;
-	document.getElementById('ji-list').textContent = LUNAR.getDayJi();
+	document.getElementById('yi-list').textContent = LUNAR.getDayYi().join(' ');
+	document.getElementById('ji-list').textContent = LUNAR.getDayJi().join(' ');
 
 	initializeEventListeners(SOLAR_DATE, LUNAR_DATE);
 }
